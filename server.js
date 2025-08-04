43a91fec81f9b536beb07f4a746c2f4b1604caf1
Dennis Richter <richter.dennis1985@gmail.com>
2025-08-04 12:59:41 +0200

    RC-003: Implemented the get /resources/id endpoint

--- server.js
+++ server.js
@@ -25,6 +25,25 @@ app.get('/resources', (req, res) => {
   }
 });
 
+app.get('/resources/:id', (req, res) => {
+  try {
+    const data = readFileSync(data_file, 'utf8');
+    const resources = JSON.parse(data);
+    
+    const resourceId = req.params.id;
+    
+    const resource = resources.find(r => String(r.id) === resourceId);
+
+    if (resource) {
+      res.json(resource);
+    } else {
+      res.status(404).json({ error: `Ressource mit der ID ${resourceId} nicht gefunden!` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Fehler beim Lesen der Ressource' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
