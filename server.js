7461ab250df93b505425916658a0ea87a9383cfc
Dennis Richter <richter.dennis1985@gmail.com>
2025-08-05 09:53:38 +0200

    RC-004: Added json middleware to parse json from requests

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ import {readFileSync}from 'fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
+// Initialisierung des Express-Servers
 const app = express();
-
 const port = 5002;
 
+// Variablen für den Dateipfad
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const data_file = path.join(__dirname, 'data', 'resources.json');
 
+// Middleware zum Parsen von JSON-Daten
+app.use(express.json());
+
+// Routen für den Server
 app.get('/', (req, res) => {
   res.send('Welcome to Resource Catalog');
 });
 
+// Route zum Abrufen aller Ressourcen
 app.get('/resources', (req, res) => {
   try {
       const data = readFileSync(data_file, 'utf8');
@@ -25,6 +31,7 @@ app.get('/resources', (req, res) => {
   }
 });
 
+// Route zum Abrufen einer Ressource nach ID
 app.get('/resources/:id', (req, res) => {
   try {
     const data = readFileSync(data_file, 'utf8');
@@ -33,17 +40,19 @@ app.get('/resources/:id', (req, res) => {
     const resourceId = req.params.id;
     
     const resource = resources.find(r => String(r.id) === resourceId);
-
+    // Überprüfen, ob die Ressource gefunden wurde
     if (resource) {
       res.json(resource);
     } else {
       res.status(404).json({ error: `Ressource mit der ID ${resourceId} nicht gefunden!` });
     }
+  // Fehlerbehandlung für das Lesen der Datei
   } catch (error) {
     res.status(500).json({ error: 'Fehler beim Lesen der Ressource' });
   }
 });
 
+// Starten des Servers
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
