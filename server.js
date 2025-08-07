80acf2b69ef32be126b438401296de834aab89a5
Dennis Richter <richter.dennis1985@gmail.com>
2025-08-07 10:26:54 +0200

    RC-011: Add dotenv for environment variables

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 // server.js
 
 // Importieren der erforderlichen Module
+// Importieren von dotenv für die Handhabung von Umgebungsvariablen
+// Diese Bibliothek wird verwendet, um Umgebungsvariablen aus einer .env-Datei zu laden
+import dotenv from 'dotenv';
+// Konfigurieren von dotenv
+dotenv.config();
+
 // Importieren von Express für die Erstellung des Servers
 import express from 'express';
 // Importieren des Ressourcen-Routers
 // Dieser Router enthält die Routen für den Zugriff auf Ressourcen
 import resourcesRouter from './routes/resources.js';
+// Importieren der Fehlerbehandlungs-Middleware
+// Diese Middleware fängt Fehler ab, die in den Routen auftreten, und sendet eine einheitliche Fehlermeldung zurück
 import {errorHandler} from './middleware/error-handler.js';
 
+
 // Festlegen des Ports für den Server
-const port = 5002;
+const port = process.env.PORT || 5002;
+
 // Initialisierung des Express-Servers
 const app = express();
 
